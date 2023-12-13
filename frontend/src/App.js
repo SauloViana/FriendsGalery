1da2856d76ea0951a0c86f00e41e76ff655f7478
Saulo Viana <sauloviana483@gmail.com>
2023-12-13 11:37:23 -0300

    Projetos Django e React criados e alterações iniciais de layout

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <div className="app-logo">logo</div>
+        <div className="app-nav">links</div>
+        <div className="app-title">title</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
